3026c3a3ccfead0985aa4da2ce4aafbba3b8e4b7
caioau012 <caio_hudak@hotmail.com>
2026-02-03 11:29:35 -0300

    Referenciando os posts do usuario

--- src/main/java/com/caiohudak/workshopmongo/domain/User.java
+++ src/main/java/com/caiohudak/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.caiohudak.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -23,6 +26,17 @@ public class User implements Serializable{
 		this.id = id;
 		this.name = name;
 		this.email = email;
+	}
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	public String getId() {
