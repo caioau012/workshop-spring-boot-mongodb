0a08d8ccae92318ace053fe68f91f1ea381a6e4d
caioau012 <caio_hudak@hotmail.com>
2026-02-03 11:40:15 -0300

    Consulta simples com query methods

--- src/main/java/com/caiohudak/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/caiohudak/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.caiohudak.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.caiohudak.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	List<Post> findByTitleContaining(String text);
 }
