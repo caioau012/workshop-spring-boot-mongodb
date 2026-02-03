c43b669fa8433db0894b965a982f7cbaae17de33
caioau012 <caio_hudak@hotmail.com>
2026-02-03 11:28:20 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/caiohudak/workshopmongo/config/Instantiation.java
+++ src/main/java/com/caiohudak/workshopmongo/config/Instantiation.java
@@ -10,10 +10,9 @@ import org.springframework.context.annotation.Configuration;
 
 import com.caiohudak.workshopmongo.domain.Post;
 import com.caiohudak.workshopmongo.domain.User;
+import com.caiohudak.workshopmongo.dto.AuthorDTO;
 import com.caiohudak.workshopmongo.repository.PostRepository;
 import com.caiohudak.workshopmongo.repository.UserRepository;
-
-import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class Instantiation implements CommandLineRunner{
@@ -21,6 +20,8 @@ public class Instantiation implements CommandLineRunner{
 
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -32,18 +33,15 @@ public class Instantiation implements CommandLineRunner{
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-	}
-
-	@Autowired
-	private PostRepository postRepository;
-	 
-	@PostConstruct 
-	public void init() throws Exception { 
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT")); 
 		postRepository.deleteAll(); 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria); 
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria); 
-		postRepository.saveAll(Arrays.asList(post1, post2)); }
-	
-}
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		 
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		postRepository.saveAll(Arrays.asList(post1, post2)); 
+	}
+
+	}
